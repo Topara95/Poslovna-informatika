70fabc1c8f1e351a4a42eccc12c71b3ec5577afb
Topara95 <jovantopolic@gmail.com>
2018-07-11 17:20:54 +0200

    Izmene na frontu

--- client/src/app/models/data.ts
+++ client/src/app/models/data.ts
@@ -17,7 +17,8 @@ const Drzava: TableInfo = {
   },
   potomci: [
     { labela: 'Naseljeno mesto', ruta: '/naseljenaMesta' },
-    { labela: 'Valuta', ruta: '/valute' }
+    { labela: 'Valuta', ruta: '/valute' },
+    { labela: 'Fizicko lice', ruta: '/fizickaLica' }
   ]
 };
 
@@ -60,6 +61,10 @@ const NaseljenoMesto: TableInfo = {
   },
   preci: [
     { labela: 'Drzave', ruta: '/drzave' },
+  ],
+  potomci: [
+    { labela: 'Fizicko lice', ruta: '/fizickaLica' },
+    { labela: 'Pravna lica', ruta: '/pravnaLica' }
   ]
 };
 
@@ -75,6 +80,114 @@ const AtributiBanke: TableInfo = {
   modelForme: {
     id: [null, [Validators.required,Validators.minLength(3),Validators.maxLength(3)]],
     swiftKod: [null, [Validators.required,Validators.minLength(8),Validators.maxLength(8)]]
+  },
+  potomci:[
+    { labela: 'Pravna lica', ruta: '/pravnaLica' }
+  ]
+}
+
+const FizickoLice: TableInfo = {
+  kolone: ['Id lica', 'Adresa', 'Id naseljenog mesta', 'Ime', 'Prezime', 'JMBG', 'Broj licne karte', 'Drzavljanstvo'],
+  atributi: ['id', 'adresa', 'naseljenomestoId', 'ime', 'prezime', 'jmbg', 'brojLicneKarte', 'drzavaId'],
+  naslov: 'Fizicka lica',
+  podaciUrl: environment.hostUrl + '/api/lica/fizickalica',
+  prikazForme: [
+    {labela: 'Adresa', tip: 'text', imeKontrole: 'adresa'},
+    {labela: 'Id naseljenog mesta', tip: 'number', imeKontrole: 'naseljenomestoId', table: NaseljenoMesto, roditeljskaKolona: 'id'},
+    {labela: 'Ime', tip: 'text', imeKontrole: 'ime'},
+    {labela: 'Prezime', tip: 'text', imeKontrole: 'prezime'},
+    {labela: 'JMBG', tip: 'number', imeKontrole: 'jmbg'},
+    {labela: 'Broj licne karte', tip: 'number', imeKontrole: 'brojLicneKarte'},
+    {labela: 'Id drzave(drzavljanstvo)', tip: 'number', imeKontrole: 'drzavaId', table: Drzava, roditeljskaKolona: 'id'},
+  ],
+  modelForme: {
+    adresa: [null,[Validators.required,Validators.maxLength(250)]],
+    naseljenomestoId: [null, Validators.required],
+    ime: [null, [Validators.required, Validators.maxLength(120)]],
+    prezime: [null, [Validators.required, Validators.maxLength(120)]],
+    jmbg: [null, [Validators.required, Validators.minLength(13),Validators.maxLength(13)]],
+    brojLicneKarte: [null, [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+    drzavaId: [null, Validators.required]
+  },
+  preci:[
+    { labela: 'Drzave', ruta: '/drzave' },
+    { labela: 'Naseljena mesta', ruta: '/naseljenaMesta' }
+  ]
+}
+
+const PravnoLice: TableInfo = {
+  kolone: ['Id lica', 'Adresa', 'Id naseljenog mesta', 'Naziv', 'Maticni broj', 'PIB', 'E-mail', 'Web','Telefon','Faks','Banka Id'],
+  atributi: ['id', 'adresa', 'naseljenomestoId', 'naziv', 'maticniBroj', 'pib', 'email', 'web', 'telefon', 'faks', 'atributiBankeId'],
+  naslov: 'Pravna lica',
+  podaciUrl: environment.hostUrl + '/api/lica/pravnalica',
+  prikazForme: [
+    {labela: 'Adresa', tip: 'text', imeKontrole: 'adresa'},
+    {labela: 'Id naseljenog mesta', tip: 'number', imeKontrole: 'naseljenomestoId', table: NaseljenoMesto, roditeljskaKolona: 'id'},
+    {labela: 'Naziv', tip: 'text', imeKontrole: 'naziv'},
+    {labela: 'Maticni Broj', tip: 'number', imeKontrole: 'maticniBroj'},
+    {labela: 'PIB', tip: 'number', imeKontrole: 'pib'},
+    {labela: 'E-mail', tip: 'text', imeKontrole: 'email'},
+    {labela: 'Web', tip: 'text', imeKontrole: 'web'},
+    {labela: 'Telefon', tip: 'text', imeKontrole: 'telefon'},
+    {labela: 'Faks', tip: 'text', imeKontrole: 'faks'},
+    {labela: 'Atributi banke(pravno lice je banka)', tip: 'text', imeKontrole: 'atributiBankeId', table: AtributiBanke, roditeljskaKolona: 'id'}
+  ],
+  modelForme: {
+    adresa: [null,[Validators.required,Validators.maxLength(250)]],
+    naseljenomestoId: [null, Validators.required],
+    naziv: [null, [Validators.required, Validators.maxLength(120)]],
+    maticniBroj: [null, [Validators.required, Validators.minLength(8), Validators.maxLength(8)]],
+    pib: [null, [Validators.required, Validators.minLength(9),Validators.maxLength(9)]],
+    email: [null, Validators.maxLength(128)],
+    web: [null, Validators.maxLength(128)],
+    telefon: [null, Validators.maxLength(20)],
+    faks: [null, Validators.maxLength(20)],
+    atributiBankeId: [null]
+  },
+  preci:[
+    { labela: 'Atributi banke', ruta: '/atributiBanaka' },
+    { labela: 'Naseljena mesta', ruta: '/naseljenaMesta' }
+  ]
+}
+
+const Banka: TableInfo = {
+  kolone: ['Id lica', 'Adresa', 'Id naseljenog mesta', 'Naziv', 'Maticni broj', 'PIB', 'E-mail', 'Web','Telefon','Faks','Banka Id'],
+  atributi: ['id', 'adresa', 'naseljenomestoId', 'naziv', 'maticniBroj', 'pib', 'email', 'web', 'telefon', 'faks', 'atributiBankeId'],
+  naslov: 'Banke',
+  podaciUrl: environment.hostUrl + '/api/lica/pravnalica/banke',
+  prikazForme: [
+  ],
+  modelForme: {
+  }
+}
+
+const Lice: TableInfo = {
+  kolone: ['Id lica', 'Adresa', 'Id naseljenog mesta'],
+  atributi: ['id', 'adresa', 'naseljenomestoId'],
+  naslov: 'Lica',
+  podaciUrl: environment.hostUrl + '/api/lica/pravnalica/lica',
+  prikazForme: [
+  ],
+  modelForme: {
+  }
+}
+
+const Racun: TableInfo = {
+  kolone: ['Id racuna', 'Broj racuna', 'Datum otvaranja', 'validan', 'Id vlasnika', 'Id banke', 'Id valute'],
+  atributi: ['id', 'brojRacuna', 'datumOtvaranja', 'validan', 'liceId', 'bankaId', 'valutaId'],
+  naslov: 'Racuni',
+  podaciUrl: environment.hostUrl + '/api/racuni',
+  prikazForme: [
+    {labela: 'Broj racuna', tip: 'text', imeKontrole: 'brojRacuna'},
+    {labela: 'Id lica', tip: 'number', imeKontrole:'liceId', table: Lice, roditeljskaKolona: 'id'},
+    {labela: 'Id banke', tip: 'number', imeKontrole:'bankaId', table: Banka, roditeljskaKolona: 'id'},
+    {labela: 'Id valuta', tip: 'number', imeKontrole:'valutaId', table: Valuta, roditeljskaKolona: 'id'},
+  ],
+  modelForme:{
+    brojRacuna: [null,[Validators.required,Validators.minLength(18),Validators.maxLength(18)]],
+    liceId: [null, Validators.required],
+    bankaId: [null, Validators.required],
+    valutaId: [null, Validators.required],
   }
 }
 
@@ -82,5 +195,8 @@ export const TableRoutes: TableRoute[] = [
   { ruta: 'drzave', labela: 'Drzave', data: Drzava },
   { ruta: 'valute', labela: 'Valute', data: Valuta },
   { ruta: 'naseljenaMesta', labela: 'Naseljena mesta', data: NaseljenoMesto },
-  { ruta: 'atributiBanaka', labela: 'Atributi banaka', data: AtributiBanke }
+  { ruta: 'atributiBanaka', labela: 'Atributi banaka', data: AtributiBanke },
+  { ruta: 'fizickaLica', labela: 'Fizicka Lica', data: FizickoLice },
+  { ruta: 'pravnaLica', labela: 'Pravna Lica', data: PravnoLice},
+  { ruta: 'racuni', labela: 'Racuni', data: Racun}
 ];
